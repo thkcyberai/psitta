cb288bfccfa9017968a335c3486e3037b498cc05
thkcyber-ai <thkcyberai@gmail.com>
2026-03-22 20:41:08 -0600

    Add S3 configuration to production secrets

--- infra/terraform/secrets.tf
+++ infra/terraform/secrets.tf
@@ -20,5 +20,11 @@ resource "aws_secretsmanager_secret_version" "app_secrets" {
     ELEVENLABS_API_KEY = var.elevenlabs_api_key
     AZURE_TTS_KEY      = var.azure_tts_key
     ANTHROPIC_API_KEY  = var.anthropic_api_key
+
+    S3_ENDPOINT_URL        = "https://s3.amazonaws.com"
+    S3_REGION              = "us-east-1"
+    S3_BUCKET_NAME         = "${var.project}-documents-prod"
+    AWS_ACCESS_KEY_ID      = var.aws_access_key_id
+    AWS_SECRET_ACCESS_KEY  = var.aws_secret_access_key
   })
 }
